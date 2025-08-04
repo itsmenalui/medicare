57d6a7f62c2276dbb0a67b2601ae08462fb7e34c
itsmenalui <niloystudent870@gmail.com>
2025-08-05 00:12:44 +0600

    asdads

--- server/index.js
+++ server/index.js
@@ -10,7 +10,7 @@ const server = http.createServer(app);
 
 const io = new Server(server, {
   cors: {
-    origin: "https://medicaree.onrender.com",
+    origin: "https://medicare4bd.netlify.app",
     methods: ["GET", "POST"],
   },
 });
